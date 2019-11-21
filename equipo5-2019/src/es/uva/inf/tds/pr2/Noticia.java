ca8c15eec0dcfa72329ca8240100a1617a05a129
Maria Robles <maria.robles.blanco@alumnos.uva.es>
2019-11-21 13:54:44 +0100

    Comenzar JavaDoc del constructor noticia

--- equipo5-2019/src/es/uva/inf/tds/pr2/Noticia.java
+++ equipo5-2019/src/es/uva/inf/tds/pr2/Noticia.java
@@ -2,8 +2,26 @@ package es.uva.inf.tds.pr2;
 
 import java.time.LocalDate;
 
+/**
+ * 
+ * @author silmont
+ * @author marrobl
+ *
+ */
 public class Noticia {
 
+	/**
+	 * Crea un objeto de tipo Noticia que tiene un titular, una fecha de publicacion,
+	 * una fuente, una url y una categoria
+	 * @param titular titular de la noticia 
+	 * @param fecha fecha de publicacion de la noticia
+	 * @param fuente nombre del medio o agencia de prensa 
+	 * @param url url donde se encuentra la noticia
+	 * @param categoria categoria a la que pertenece la noticia, puede ser
+	 * nacional, internacional, sociedad, economia, deporte o cultura
+	 * @pre.condition titular 
+	 * 
+	 */
 	public Noticia(String titular, LocalDate fecha, String fuente, String url, CategoriaNoticia categoria) {
 		// TODO Auto-generated constructor stub
 	}

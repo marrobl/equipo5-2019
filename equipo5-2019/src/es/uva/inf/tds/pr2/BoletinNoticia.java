e9a427015b0840327e7828ee5c304612b7be4557
Silvia Montero <silviacarmen.montero@alumnos.uva.es>
2019-11-25 19:18:43 +0100

    Creacion javadoc constructor BoletinNoticia y getListaNoticias

--- equipo5-2019/src/es/uva/inf/tds/pr2/BoletinNoticia.java
+++ equipo5-2019/src/es/uva/inf/tds/pr2/BoletinNoticia.java
@@ -2,12 +2,34 @@ package es.uva.inf.tds.pr2;
 
 import java.util.ArrayList;
 
+/**
+ * Clase que representa un boletin de noticias compuesto por varias noticias
+ * @author silmont
+ * @author marrobl
+ *
+ */
+
 public class BoletinNoticia {
+	
+	/**
+	 * Crea un objeto de tipo Boletin Noticia que contiene una lista de Noticias.
+	 * @param listaNoticias lista de noticias que pertenecen inicialmente al boletín
+	 * 
+	 * @pre.condition {@code listaNoticias!=null}
+	 * 
+	 * @post.condition {@code listaNoticias.equals(this.getListaNoticias())
+	 * 
+	 * @throws IllegalArgumentException cuando no se cumplen las precondiciones
+	 */
 
 	public BoletinNoticia(ArrayList<Noticia> listaNoticias) {
 		// TODO Auto-generated constructor stub
 	}
 
+	/**
+	 * Consulta la lista de noticias pertenecientes al boletin
+	 * @return lista de noticias
+	 */
 	public ArrayList<Noticia> getListaNoticias() {
 		// TODO Auto-generated method stub
 		return null;
--- equipo5-2019/testsrc/es/uva/inf/tds/pr2/BoletinNoticiaTDDTest.java
+++ equipo5-2019/testsrc/es/uva/inf/tds/pr2/BoletinNoticiaTDDTest.java
@@ -1,6 +1,10 @@
 package es.uva.inf.tds.pr2;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
 
@@ -8,6 +12,18 @@ class BoletinNoticiaTDDTest {
 
 	@Test
 	void testTDDConstructorBoletinNoticia() {
+		LocalDate fecha = LocalDate.of(2012, 12, 12);
+		Noticia noticia1 = new Noticia("Titular 1", fecha, "Fuente de la noticia", "URL de la noticia", CategoriaNoticia.NACIONAL);
+		Noticia noticia2 = new Noticia("Titular 2", fecha, "Fuente de la noticia", "URL de la noticia", CategoriaNoticia.NACIONAL);	
+		ArrayList<Noticia> listaNoticias = new ArrayList<Noticia>();
+		listaNoticias.add(noticia1);
+		listaNoticias.add(noticia2);
+		BoletinNoticia boletin = new BoletinNoticia(listaNoticias);
+		Noticia[] arrayNoticias = new Noticia[listaNoticias.size()];
+		arrayNoticias = listaNoticias.toArray(arrayNoticias);
+		fail("Not yet implemented");
+		assertArrayEquals(boletin.getListaNoticias().toArray(), arrayNoticias);
+		assertNotNull(boletin.getListaNoticias());
 		
 	}
 

5fae2b172fa13c8fc91109c966661d44c1c82da4
Silvia Montero <silviacarmen.montero@alumnos.uva.es>
2019-11-25 19:01:49 +0100

    Creacion test constructor de BoletinNoticia junto con el stub asociado.

--- equipo5-2019/src/es/uva/inf/tds/pr2/BoletinNoticia.java
+++ equipo5-2019/src/es/uva/inf/tds/pr2/BoletinNoticia.java
@@ -1,5 +1,16 @@
 package es.uva.inf.tds.pr2;
 
+import java.util.ArrayList;
+
 public class BoletinNoticia {
+
+	public BoletinNoticia(ArrayList<Noticia> listaNoticias) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public ArrayList<Noticia> getListaNoticias() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
